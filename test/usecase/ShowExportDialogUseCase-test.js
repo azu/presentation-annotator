105ec1483c17ae46a5aae4aef1de008092eb4d11
azu <azuciao@gmail.com>
2016-04-04 11:36:12 +0900

    chore(test): fix test case description

--- test/usecase/ShowExportDialogUseCase-test.js
+++ test/usecase/ShowExportDialogUseCase-test.js
@@ -7,7 +7,7 @@ import DocumentService from "../../src/js/domain/Document/DocumentService";
 import {DocumentRepository} from "../../src/js/infra/DocumentRepository";
 describe("ShowExportDialogUseCase", function () {
     context("execute", function () {
-        it("should dispatch with document", function (done) {
+        it("should dispatch with output", function (done) {
             const documentRepository = new DocumentRepository();
             const document = new Document();
             const expectedOutput = DocumentService.stringify(document);
