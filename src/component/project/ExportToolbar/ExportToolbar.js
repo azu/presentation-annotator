3dceb4733cbb950a4465667f0927c6754c310908
azu <azuciao@gmail.com>
2016-04-06 16:50:19 +0900

    feat(ExportToolbar): add hover style

--- src/component/project/ExportToolbar/ExportToolbar.js
+++ src/component/project/ExportToolbar/ExportToolbar.js
@@ -1,6 +1,7 @@
 // LICENSE : MIT
 "use strict";
 const React = require("react");
+import HoverBlock from "../../uikit/HoverBlock/HoverBlock";
 import AppContextRepository from "../../../AppContextRepository";
 import ExportUseCaseController from "../../../js/UseCaseController/ExportUseCaseController";
 export default class ExportToolbar extends React.Component {
@@ -8,14 +9,14 @@ export default class ExportToolbar extends React.Component {
         const ShowExportBox = () => {
             AppContextRepository.context.execute(ExportUseCaseController.ShowExportDialogUseCase());
         };
-        return <div className="ExportToolbar">
-            <ul>
-                <li>
+        return <HoverBlock className="ExportToolbar">
+            <ul className="ExportToolbar-list">
+                <li className="ExportToolbar-listItem">
                     <buttom className="ExportToolbar-exportButton"
                             onClick={ShowExportBox}>Output JSON
                     </buttom>
                 </li>
             </ul>
-        </div>
+        </HoverBlock>
     }
 }
