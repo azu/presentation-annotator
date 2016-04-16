a841d4a4144b55d93e688d0ed142ebca5b8f2e48
azu <azuciao@gmail.com>
2016-04-16 15:32:44 +0900

    docs(dispatcher): add JSDoc to dispatcher

--- src/js/framework/Dispatcher.js
+++ src/js/framework/Dispatcher.js
@@ -11,10 +11,12 @@ export const ON_DID_EXECUTE_EACH_USECASE = "ON_DID_EXECUTE_EACH_USECASE";
  * @property {String} type The event type to dispatch.
  */
 /**
- * Dispatcher is the central event bus system.
+ * Dispatcher is the **central** event bus system.
  *
- * onDispatch
- * dispatch
+ * also have these method.
+ *
+ * - onDispatch(payloadHandler): Function
+ * - dispatch(payload): void
  *
  * Almost event pass the (on)dispatch.
  *
@@ -22,11 +24,12 @@ export const ON_DID_EXECUTE_EACH_USECASE = "ON_DID_EXECUTE_EACH_USECASE";
  *
  * Q. Why use payload object instead emit(key, ...args).
  * A. It is for optimization and limitation.
- * If apply emit style, we cast ...args for passing other dispatcher at everytime.
+ * If apply emit style, we cast ...args for passing other dispatcher at every time.
  */
 export default class Dispatcher extends CoreEventEmitter {
     // Public global hook point
     /**
+     * called the {@link handler} with useCase when the useCase will do.
      * @param {function(useCase: UseCase)} handler
      */
     onWillExecuteEachUseCase(handler) {
@@ -34,17 +37,26 @@ export default class Dispatcher extends CoreEventEmitter {
     }
 
     /**
+     * called the {@link handler} with useCase when the useCase is done.
      * @param {function(useCase: UseCase)} handler
      */
     onDidExecuteEachUseCase(handler) {
         this.on(ON_DID_EXECUTE_EACH_USECASE, handler);
     }
 
+    /**
+     * dispatch the method when {@link useCase} will do.
+     * @param {UseCase} useCase
+     */
     dispatchWillExecuteUseCase(useCase) {
         this.emit(ON_WILL_EXECUTE_EACH_USECASE, useCase);
 
     }
 
+    /**
+     * dispatch the method when {@link useCase} is done.
+     * @param {UseCase} useCase
+     */
     dispatchDidExecuteUseCase(useCase) {
         this.emit(ON_DID_EXECUTE_EACH_USECASE, useCase);
     }
